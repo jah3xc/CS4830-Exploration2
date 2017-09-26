66ab5a85eebe2ef583ee42345663f8e6c649753c
James Hurt <jah3xc@mail.missouri.edu>
2017-09-25 23:12:16 -0500

    Working form

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ $connection = pg_connect(CONNECTION_STRING);
 if(!$connection) {
     die("No connection");
 }
+
+if(isset($_POST["type"])) {
+	$insert = pg_prepare($connection, "in", "SELECT create_transaction($1,$2,$3)");
+	pg_execute($connection, "in", array($_POST["type"], $_POST["amount"], $_POST["name"]));
+	header("Refresh:0");
+}
 $query = pg_prepare($connection, "query", "SELECT * FROM transaction");
 $query = pg_execute($connection, "query", array());
 $result = pg_fetch_all($query);
@@ -18,11 +24,32 @@ $result = pg_fetch_all($query);
   <link rel="stylesheet" href="bootstrap.min.css" />
   <script src="jquery.min.js"></script>
   <script src="bootstrap.min.js"></script>
+  <script>
+	$(function() {
+		$('label').addClass("control-label");
+		$('input').addClass("form-control").attr("required", "required");
+		$('button').addClass("btn btn-primary");
+	});
+  </script>
 </head>
 
 <body>
 
 <h1 class="jumbotron text-center">PostgreSQL</h1>
+<div class="container-fluid">
+<div class="col-md-4">
+    <form action="" method="POST" class="form">
+	<label>Type</label>
+	<input name="type" />
+	<label>Amount</label>
+	<input type="number" name="amount" />
+	<label>Name</label>
+	<input name="name" />
+<br>
+	<button type="submit">Submit</button>
+    </form>
+</div>
+<div class="col-md-8">
 <div class="container" style="max-height:600px;overflow:auto;border:solid .5px;">
     <table class="table">
         <tr>
@@ -42,6 +69,7 @@ $result = pg_fetch_all($query);
 
     </table>
 </div>
-
+</div>
+</div>
 </body>
 </html>
